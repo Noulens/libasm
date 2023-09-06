02b37b693759d57febf69855322f071885d480ac
tnoulens <tnoulens@42.fr>
2023-09-06 09:20:23 -0500

    ft_list_sort

--- ft_list_size.s
+++ ft_list_size.s
@@ -5,14 +5,6 @@ section .bss
 section .text
 	global  ft_list_size
 ft_list_size:
-<<<<<<< HEAD
-	test	rdi, rdi
-	jz		.done
-	mov		rdi, [rdi + 8]
-	call	ft_list_size
-	add		rax, 1
-	ret
-=======
     ; If lst is NULL, return 0
     test rdi, rdi
     jz .done
@@ -22,7 +14,6 @@ ft_list_size:
     call ft_list_size              ; Call ft_lstsize recursively
     add rax, 1                   ; Add 1 to the result
     ret
->>>>>>> refs/remotes/origin/main
 
 .done:
 	xor	rax, rax                 ; Return 0
